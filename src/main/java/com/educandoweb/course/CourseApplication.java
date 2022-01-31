146aa31f7271bfc8bbc0c3466c58d60c6668ef94
Beatriz Gomes <beatrizg00@hotmail.com>
2022-01-31 14:13:35 -0300

    OrderStatus enum

--- src/main/java/com/educandoweb/course/CourseApplication.java
+++ src/main/java/com/educandoweb/course/CourseApplication.java
@@ -2,6 +2,9 @@ package com.educandoweb.course;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class CourseApplication {
@@ -9,5 +12,18 @@ public class CourseApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CourseApplication.class, args);
 	}
+	
+	@Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+            	registry.addMapping("/**")
+				.allowedOrigins("*")
+				.allowedHeaders("*")
+				.allowedMethods("GET", "PUT", "POST", "PATCH", "DELETE", "OPTIONS");
+            }
+        };
+    }
 
 }
--- src/main/java/com/educandoweb/course/resources/UserResource.java
+++ src/main/java/com/educandoweb/course/resources/UserResource.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,13 +13,15 @@ import org.springframework.web.bind.annotation.RestController;
 import com.educandoweb.course.entities.User;
 import com.educandoweb.course.services.UserService;
 
+
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 	
 	@Autowired
 	private UserService service;
-
+	
+	@CrossOrigin
 	@GetMapping
 	public ResponseEntity<List<User>> findAll() {
 		List<User> list = service.findAll();
